347260d2bb4431557bbd67e167a81c2e7138e698
Abdon13 <abdonlc0313@gmail.com>
2025-08-15 11:10:01 -0600

    Routing dinámico

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import { Category } from "@/app/generated/prisma"
 
 type CategoryIconProps = {
@@ -18,9 +19,11 @@ export default function CategoryIcon({category}: CategoryIconProps) {
             />
         </div>
 
-        <p className="text-xl font-bold">
-            {category.name}</p>
-            
+        <Link 
+            className="text-xl font-bold"
+            href={`/order/${category.slug}`}
+        >{category.name}</Link>
+
     </div>
   )
 }
