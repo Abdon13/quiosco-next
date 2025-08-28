669cfbf5ca956dfdfd88c6ac89b189d1eb860bfd
Abdon13 <abdonlc0313@gmail.com>
2025-08-28 09:53:37 -0600

    Ajustando el formulario de edición y llenando los campos automaticamente

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import EditProductForm from "@/components/products/EditProductForm"
+import ProductForm from "@/components/products/ProductForm"
+import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
 import { notFound } from "next/navigation"
 
@@ -18,6 +21,14 @@ export default async function EditProductsPage({params}: {params: {id: string}})
   const product = await getProductById(+params.id)
   console.log(product)
   return (
-    <div>page</div>
+    <>
+      <Heading>Editar Producto; {product.name}</Heading>
+
+      <EditProductForm>
+        <ProductForm
+          product={product}
+        />
+      </EditProductForm>
+    </>
   )
 }
